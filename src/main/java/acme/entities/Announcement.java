8d8dec377483873837ecc98f5fc88f18c0e5848b
Diego <diewentdiaz@gmail.com>
2022-03-18 21:19:21 +0100

    Task-219: Cambiado critical a boolean primitivo

--- src/main/java/acme/entities/Announcement.java
+++ src/main/java/acme/entities/Announcement.java
@@ -40,7 +40,7 @@ public class Announcement extends AbstractEntity{
 		protected String body;
 		
 		@NotNull
-		protected Boolean critical;
+		protected boolean critical;
 
 		@URL
 		protected String link;
