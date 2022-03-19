0cf3006b1b3c1eb9455297dd8618c619c1ed982c
Manuel <mansanrod4@alum.us.es>
2022-03-19 14:12:16 +0100

    Task 222: Patronage report Fix
    Intento de generar de forma derivada el sequence_number

--- src/main/java/acme/entities/PatronageReport.java
+++ src/main/java/acme/entities/PatronageReport.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -22,16 +23,13 @@ import lombok.NoArgsConstructor;
 @Data
 @EqualsAndHashCode(callSuper=true)
 @NoArgsConstructor
-public class PatronageReport extends AbstractEntity{/**
-	 * 
-	 */
+public class PatronageReport extends AbstractEntity{
+
+	// Serialisation identifier -----------------------------------------------
+
 	private static final long serialVersionUID = 7607756024273072128L;
 	
-	@ManyToOne(optional=false)
-	protected Patronage patronage;
-	
-	@NotBlank
-	protected String sequenceNumber;
+	// Attributes -------------------------------------------------------------
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
@@ -43,6 +41,31 @@ public class PatronageReport extends AbstractEntity{/**
 	protected String memorandum;
 	
 	@URL
-	protected String link;
+	protected String info;
+//	
+//	@NotNull
+//	@Digits( integer = 4, fraction = 0)
+//	protected Integer serialNumber;
+//	
+//	
+	//pattern “〈patronage-code〉:〈serial-number〉
+	@NotBlank
+	protected String sequenceNumber;
+
+	// Derived attributes -----------------------------------------------------
+	
+	//pattern “〈patronage-code〉:〈serial-number〉
+//	@NotBlank
+//	protected String sequenceNumber(){
+//		return "〈"+this.patronage.getCode() +"〉:〈"+this.serialNumber.toString()+"〉";
+//	}
+//	
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Patronage patronage;
+	
 
 }
