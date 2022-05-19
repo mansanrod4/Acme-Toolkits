a43c29bd1fff09de66024240b6f01c67b36ea284
olisanrod@alum.us.es <olisanrod@alum.us.es>
2022-05-19 12:25:32 +0200

    Task 422: Test update systemConfiguration

--- src/test/java/acme/testing/administrator/systemConfiguration/AdministratorSystemConfigurationUpdateTest.java
+++ src/test/java/acme/testing/administrator/systemConfiguration/AdministratorSystemConfigurationUpdateTest.java
@@ -11,40 +11,41 @@ public class AdministratorSystemConfigurationUpdateTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/systemConfiguration/update-system-configuration.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String reference, final String title, final String deadline, final String salary, final String score, final String moreInfo, final String description) {
-		super.signIn("employer1", "employer1");
+	public void positiveTest(final int recordIndex, final String systemCurrency, final String acceptedCurrencies, final String strongSpamTerms, final String strongSpamThreshold, final String weakSpamTerms, final String weakSpamThreshold) {
+		super.signIn("administrator", "administrator");
 
-		super.clickOnMenu("Employer", "List my jobs");
+		super.clickOnMenu("Administrator", "System Details");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
-		super.checkColumnHasValue(recordIndex, 0, reference);
+		super.checkColumnHasValue(recordIndex, 0, systemCurrency);
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
-		super.fillInputBoxIn("reference", reference);
-		super.fillInputBoxIn("title", title);
-		super.fillInputBoxIn("deadline", deadline);
-		super.fillInputBoxIn("salary", salary);
-		super.fillInputBoxIn("score", score);
-		super.fillInputBoxIn("moreInfo", moreInfo);
-		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("systemCurrency", systemCurrency);
+		super.fillInputBoxIn("acceptedCurrencies", acceptedCurrencies);
+		super.fillInputBoxIn("deadline", strongSpamTerms);
+		super.fillInputBoxIn("salary", strongSpamThreshold);
+		super.fillInputBoxIn("score", weakSpamTerms);
+		super.fillInputBoxIn("moreInfo", weakSpamThreshold);
 		super.clickOnSubmit("Update");
 
 		super.checkListingExists();
 		super.sortListing(0, "asc");
-		super.checkColumnHasValue(recordIndex, 0, reference);
-		super.checkColumnHasValue(recordIndex, 1, deadline);
-		super.checkColumnHasValue(recordIndex, 2, title);
+		super.checkColumnHasValue(recordIndex, 0, systemCurrency);
+		super.checkColumnHasValue(recordIndex, 1, acceptedCurrencies);
+		super.checkColumnHasValue(recordIndex, 2, strongSpamTerms);
+		super.checkColumnHasValue(recordIndex, 3, strongSpamThreshold);
+		super.checkColumnHasValue(recordIndex, 4, weakSpamTerms);
+		super.checkColumnHasValue(recordIndex, 5, weakSpamThreshold);
 
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
-		super.checkInputBoxHasValue("reference", reference);
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("deadline", deadline);
-		super.checkInputBoxHasValue("salary", salary);
-		super.checkInputBoxHasValue("score", score);
-		super.checkInputBoxHasValue("moreInfo", moreInfo);
-		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("systemCurrency", systemCurrency);
+		super.checkInputBoxHasValue("acceptedCurrencies", acceptedCurrencies);
+		super.checkInputBoxHasValue("strongSpamTerms", strongSpamTerms);
+		super.checkInputBoxHasValue("strongSpamThreshold", strongSpamThreshold);
+		super.checkInputBoxHasValue("weakSpamTerms", weakSpamTerms);
+		super.checkInputBoxHasValue("weakSpamThreshold", weakSpamThreshold);
 
 		super.signOut();
 	}
