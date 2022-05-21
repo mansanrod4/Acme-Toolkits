2ce0a4018d6f15619756ae649c5ed23627bf6071
Diego <diewentdiaz@gmail.com>
2022-05-21 21:08:50 +0200

    Task-405: Eliminado spam detector

--- src/main/java/acme/features/inventor/item/tool/InventorToolCreateService.java
+++ src/main/java/acme/features/inventor/item/tool/InventorToolCreateService.java
@@ -3,7 +3,6 @@ package acme.features.inventor.item.tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.components.configuration.SystemConfiguration;
 import acme.entities.toolkits.Item;
 import acme.entities.toolkits.ItemType;
 import acme.features.administrator.systemConfiguration.AdministratorSystemConfigurationRepository;
@@ -13,7 +12,6 @@ import acme.framework.controllers.Errors;
 import acme.framework.controllers.Request;
 import acme.framework.services.AbstractCreateService;
 import acme.roles.Inventor;
-import notenoughspam.SpamDetector;
 
 @Service
 public class InventorToolCreateService implements AbstractCreateService<Inventor, Item>{
@@ -69,30 +67,6 @@ public class InventorToolCreateService implements AbstractCreateService<Inventor
 		if(!errors.hasErrors("code")) {
 			final Item existing=this.repository.getItemByCode(entity.getCode());
 			errors.state(request, existing == null ||  entity.getId() == existing.getId(), "code", "inventor.item.form.error.duplicated");
-		}
-		
-final SystemConfiguration sysConfig = this.systemConfigRepository.findSystemConfiguration();
-		
-		final Double strongSpamThreshold = sysConfig.getStrongSpamThreshold();
-		final String[] strongSpamWords = sysConfig.getStrongSpamTerms().split(",");
-		
-		final Double weakSpamThreshold = sysConfig.getWeakSpamThreshold();
-		final String[] weakSpamWords = sysConfig.getWeakSpamTerms().split(",");
-		
-		final SpamDetector spamDetector = new SpamDetector(strongSpamThreshold, weakSpamThreshold, strongSpamWords, weakSpamWords);
-		
-		
-		if (!errors.hasErrors("name")) {
-			final String name = entity.getName();
-			errors.state(request, spamDetector.stringHasSpam(name), "name", "inventor.item.form.error.spam");
-		}
-		
-		if (!errors.hasErrors("technology")) {
-			errors.state(request, spamDetector.stringHasSpam(entity.getDescription()), "technology", "inventor.item.form.error.spam");
-		}
-		
-		if (!errors.hasErrors("description")) {
-			errors.state(request, spamDetector.stringHasSpam(entity.getDescription()), "description", "inventor.item.form.error.spam");
 		}
 		
 		if (!errors.hasErrors("retailPrice")) {
