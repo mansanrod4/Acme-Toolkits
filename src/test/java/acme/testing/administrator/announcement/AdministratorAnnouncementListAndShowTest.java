52a9c220358e23f18dc23376d1887b62ce45d300
Aisha <aisqazcev@alum.us.es>
2022-05-22 18:04:11 +0200

    Task

--- src/test/java/acme/testing/administrator/announcement/AdministratorAnnouncementListAndShowTest.java
+++ src/test/java/acme/testing/administrator/announcement/AdministratorAnnouncementListAndShowTest.java
@@ -10,7 +10,7 @@ import acme.testing.TestHarness;
 public class AdministratorAnnouncementListAndShowTest extends TestHarness{
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/announcement/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/administrator/announcement/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveAnnouncementTest(final int recordIndex, final String title, final String moment, 
 		final String body, final String critical, final String link) {
@@ -24,10 +24,11 @@ public class AdministratorAnnouncementListAndShowTest extends TestHarness{
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, moment);
 		super.checkColumnHasValue(recordIndex, 2, body);
+		
+		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("body", body);
-		super.checkInputBoxHasValue("critical", critical);
+		super.checkInputBoxHasValue("body", body);	
 		super.checkInputBoxHasValue("link", link);
 		super.signOut();
 		
@@ -37,16 +38,16 @@ public class AdministratorAnnouncementListAndShowTest extends TestHarness{
 	@Order(30)
 	public void hackingTest() {
 		
-		super.navigate("/administrator/systemConfiguration/");
+		super.navigate("/administrator/announcement/list");
 		super.checkPanicExists();
 		
 		super.signIn("patron1", "patron1");
-		super.navigate("/administrator/systemConfiguration/");
+		super.navigate("/administrator/announcement/list");
 		super.checkPanicExists();
 		super.signOut();
 
 		super.signIn("inventor1", "inventor1");
-		super.navigate("/administrator/systemConfiguration/");
+		super.navigate("/administrator/announcement/list");
 		super.checkPanicExists();
 		super.signOut();
 	}
