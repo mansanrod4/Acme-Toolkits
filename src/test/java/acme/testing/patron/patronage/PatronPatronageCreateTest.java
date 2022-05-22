81dd8b71ad72a70dbcb349b7ec8c4acd694f16e0
Manuel Sánchez Rodríguez <mansanrod4@alum.us.es>
2022-05-22 19:20:43 +0200

    Task 419: create patronage negative

--- src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
@@ -1,47 +1,79 @@
+
 package acme.testing.patron.patronage;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.openqa.selenium.By;
 import org.springframework.core.annotation.Order;
 
-import acme.framework.testing.BrowserDriver;
 import acme.testing.TestHarness;
 
-public class PatronPatronageCreateTest extends TestHarness{
+public class PatronPatronageCreateTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/patron/patronage/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int patronageIndex, final String code,
-		final String inventorFullName,
-		final String startDate,	final String endDate, final String legalStuff, final String budget, final String info) {
-	
+	public void positiveTest(final int patronageIndex, final String code, final String startDate, final String endDate, final String legalStuff, final String budget, final String info, final String inventorFullName,
+		final String inventorEmail, final String inventorCompany, final String inventorStatement, final String inventorInfo) {
+
 		super.signIn("patron3", "patron3");
-		
+
 		super.navigateHome();
-		
+
 		super.clickOnMenu("Patron", "My Patronages");
 		super.clickOnButton("Create");
-		
-		super.fillInputBoxIn("code", code);
-		final BrowserDriver driver = super.getDriver();
-        driver.locateOne(By.id("inventor3")).click();
-        driver.locateOne(By.id("inventor3")).click();
 
-        
+		super.fillInputBoxIn("code", code);
+
 		super.fillInputBoxIn("startDate", startDate);
 		super.fillInputBoxIn("endDate", endDate);
 		super.fillInputBoxIn("legalStuff", legalStuff);
+		super.fillInputBoxIn("budget", budget);
 		super.fillInputBoxIn("info", info);
-		
+
 		super.clickOnSubmit("Create");
-		this.clickOnListingRecord(patronageIndex);
-		
+
+		super.sortListing(0, "asc");
+
+		this.clickOnListingRecord(patronageIndex); //1
+
 		super.checkInputBoxHasValue("code", code);
-		//no terminado
-		
-		
+		super.checkInputBoxHasValue("startDate", startDate);
+		super.checkInputBoxHasValue("endDate", endDate);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("info", info);
+		super.checkInputBoxHasValue("inventorFullName", inventorFullName);
+		super.checkInputBoxHasValue("inventorEmail", inventorEmail);
+		super.checkInputBoxHasValue("inventorCompany", inventorCompany);
+		super.checkInputBoxHasValue("inventorStatement", inventorStatement);
+		super.checkInputBoxHasValue("inventorInfo", inventorInfo);
+
+		super.signOut();
+
 	}
 	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeTest(final int patronageIndex, final String code, final String startDate, final String endDate, final String legalStuff, final String budget, final String info, final String inventorFullName,
+		final String inventorEmail, final String inventorCompany, final String inventorStatement, final String inventorInfo) {
+
+		super.signIn("patron3", "patron3");
+
+		super.navigateHome();
+
+		super.clickOnMenu("Patron", "My Patronages");
+		super.clickOnButton("Create");
+
+		super.fillInputBoxIn("code", code);
+
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("endDate", endDate);
+		super.fillInputBoxIn("legalStuff", legalStuff);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("info", info);
+
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+	}
+
 }
